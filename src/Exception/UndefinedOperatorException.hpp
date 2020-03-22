58456b3954e80ae78ea4ea61c19c3e7405456207
farisfadhilah <farisfadhilah27@gmail.com>
2020-03-22 08:10:17 +0700

    nambah komentar exception

--- src/Exception/UndefinedOperatorException.hpp
+++ src/Exception/UndefinedOperatorException.hpp
@@ -3,14 +3,26 @@
 
 #include "BaseException.hpp"
 
+/**
+ * @brief UndefinedOperatorException class
+ * class yang berisi method dan atribut berguna untuk mengevaluasi operator tidak terdefinisi 
+ * pada ekspresi
+ **/
 class UndefinedOperatorException : public BaseException{
-    //mengirimkan pesan kesalahan jika operator lebih dari satu
     private:
         string opertor;
     public :
+    /**
+	 * @brief memeriksa operator pada suatu ekspresi
+	 * @param opertor
+	 **/
         UndefinedOperatorException(string opertor){
             this -> opertor = opertor;
         }
+    /**
+	 * @brief getMessage untuk menampilkan pesan kesalahan
+     * @return
+	 **/
         string getMessage(){
             string  ret = this->opertor + " merupakan operator yang tidak terdefinisi";
             return ret;
