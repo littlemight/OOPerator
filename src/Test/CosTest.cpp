633487b4ec9df048caed9ea69f3e2b0406389fc8
Michel Fang <mfang555@gmail.com>
2020-03-22 18:38:59 +0700

    add queue test

--- src/Test/CosTest.cpp
+++ src/Test/CosTest.cpp
@@ -1,6 +1,6 @@
 #include "all.hpp"
 
-TEST_CASE("cos") {
+TEST_CASE("Cos") {
     REQUIRE(Parser::evalUnaryOp(60, "cos") == Approx(0.5));
     REQUIRE(Parser::evalUnaryOp(30, "cos") == Approx(0.8660254041));
     REQUIRE_FALSE(Parser::evalUnaryOp(90, "cos") == 1);
--- src/Test/QueueTest.cpp
+++ src/Test/QueueTest.cpp
@@ -1,8 +1,35 @@
 #include "all.hpp"
 
+bool qNotEmpty(queue<double> &q) {
+    if (q.empty()) throw "Custom empty exception, STL Queue doesn\'t throw an exception when we empty an empty queue";
+    return true;
+}
+
+
 TEST_CASE("Queue") {
     queue<double> q;
-    REQUIRE(q.empty());
-    q.push(1);
-    REQUIRE(q.front() == 1);
+
+    SECTION("Push and see front") {
+        while (!q.empty()) {
+            q.pop();
+        }
+        q.push(1);
+        REQUIRE(q.front() == 1);
+    }
+
+    SECTION("Push another, pop, and see pushed value") {
+        q.push(1);
+        REQUIRE(q.front() == 1);
+        q.push(2);
+        q.pop();
+        REQUIRE(q.front() == 2);
+        q.pop();
+    }
+
+    SECTION("Pop when empty, custom exception") {
+        q.push(135);
+        REQUIRE(qNotEmpty(q) == true);
+        q.pop();
+        REQUIRE_THROWS_AS(qNotEmpty(q) == false, const char *);
+    }
 }
